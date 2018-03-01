c5d130171f6615cdb24b268413ff2c743f445410
CaiWenqing <qianyucha@foxmail.com>
2018-03-01 16:08:36 +0800

    Initial commit

---
+++ app/src/main/java/com/cwq/tinkertest/MainActivity.java
@@ -0,0 +1,48 @@
+package com.cwq.tinkertest;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.tencent.tinker.lib.tinker.TinkerInstaller;
+
+public class MainActivity extends AppCompatActivity {
+    TextView tv_msg;
+    Button btn_fix;
+
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+            "android.permission.READ_EXTERNAL_STORAGE",
+            "android.permission.WRITE_EXTERNAL_STORAGE"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tv_msg = findViewById(R.id.tv_message);
+        btn_fix = findViewById(R.id.btn_fix);
+
+        tv_msg.setText("这是修改后的的的的的的带的的文字");
+        btn_fix.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //检测是否有写的权限
+                int permission = ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+                if (permission != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(MainActivity.this, PERMISSIONS_STORAGE, REQUEST_EXTERNAL_STORAGE);
+                } else {
+                    TinkerInstaller.onReceiveUpgradePatch(getApplicationContext(),
+                            Environment.getExternalStorageDirectory().getAbsolutePath() + "/patch_signed.apk");
+                }
+            }
+        });
+    }
+}
